8b377a0d76c0bbf0cad4f56d9eec8fba314529f0
rozievich <blackcoder459@outlook.com>
2023-08-24 04:12:26 -0400

    Allowed Host & DEBUG

--- config/settings.py
+++ config/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-gi1f0mlg15v0h%pk_7(5+qrbnfi(#h_bcvw=0*-f0oc!ysi+c^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
